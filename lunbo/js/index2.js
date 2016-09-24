11d8a491e939636ca2b1ab35dffa19cbb9d8bf4c
kimigo168 <462494169@qq.com>
2016-09-24 20:50:55 +0800

    modify

--- lunbo/js/index2.js
+++ lunbo/js/index2.js
@@ -1,39 +1,37 @@
-function lunBo(imgWid,imgNum) {
-    //获取一堆元素
-    var container = document.getElementsByClassName('container')[0];
-    var imgList = document.getElementsByClassName('imgList')[0];
-    var images = imgList.children;
-    var dotList = document.getElementsByClassName('dotList')[0];
-    var dots = dotList.children;
-    var prev = document.getElementsByClassName('prev')[0];
-    var next = document.getElementsByClassName('next')[0];
-    var timer;//这是一个定时器
-    var imgWid;
-    var imgNum;
-    //点击事件
-    prev.onclick = function(){//如果是function(imgWid,imgNum)错误，
-        //直接就执行了方法function(),而没有正确把这个方法对象赋给btn.onclick事件. 
+function Lunbo(imgWid,imgNum){
+    var container = $(".container");
+    var imgList = $(".imgList");
+    var dotList = $(".dotList");
+    var dots = dotList.children();
+    var prev = $(".prev");
+    var next = $(".next");
+    var imgWid,imgNum;
+    var timer;//定时器
+    // 左右按钮点击
+    prev.click(function(){
         leftMove(imgWid,imgNum);
         tab(imgWid,imgNum);
-    }
-    next.onclick = function(){
+    });
+    next.click(function(){
         rightMove(imgWid,imgNum);
         tab(imgWid,imgNum);
-    }
+    });
     //点击向左移动
     function leftMove(imgWid,imgNum){
-        imgList.style.left = parseInt(imgList.style.left) + imgWid +'px';
+        var left = parseInt(imgList.css("left")) + imgWid +'px';
+        imgList.css('left',left);
         //判断一下点击到头的情况
-        if(parseInt(imgList.style.left) > 0){
-            imgList.style.left = -(imgWid*(imgNum-1)) + 'px';
+        if(parseInt(imgList.css("left") ) > 0){
+            imgList.css("left",-(imgWid*(imgNum-1)) + 'px');
         }
     }
     //点击向右移动
     function rightMove(imgWid,imgNum){
-        imgList.style.left = parseInt(imgList.style.left) - imgWid + 'px';
+        var left = parseInt(imgList.css("left")) - imgWid + 'px';
+        imgList.css('left',left);
         //判断一下移到头的情况
-        if(parseInt(imgList.style.left) < -(imgWid*(imgNum-1))){
-            imgList.style.left = 0 + 'px';
+        if(parseInt(imgList.css("left")) < -(imgWid*(imgNum-1))){
+            imgList.css("left", 0 + 'px');
         }
     }
     function dotHover(imgWid,imgNum){
@@ -49,7 +47,7 @@ function lunBo(imgWid,imgNum) {
                 //让点的index对应图片的index
                 for (var i = 0; i < imgNum; i++) {
                     if (dots[i].className == 'on') {
-                        imgList.style.left = -imgWid * i + 'px';
+                        imgList.css("left",(-imgWid * i + 'px'));
                     }
                 }
 
@@ -59,7 +57,7 @@ function lunBo(imgWid,imgNum) {
     dotHover(imgWid,imgNum);
     //点击时候正好对应这个点
     function tab(imgWid,imgNum) {
-        var index = Math.abs(parseInt(imgList.style.left) / imgWid);
+        var index = Math.abs(parseInt(imgList.css("left")) / imgWid);
         for (var i = 0; i < imgNum; i++) {
             if (i !== index) {
                 if (dots[i].className == 'on') {
@@ -73,7 +71,7 @@ function lunBo(imgWid,imgNum) {
     //设置个定时器自动轮播,实际上就是触发点击事件
     function go() {
         timer = setInterval(function () {
-            next.onclick(imgWid,imgNum);
+            next.click();
         }, 3000)
     }
     go();
@@ -84,5 +82,4 @@ function lunBo(imgWid,imgNum) {
     container.onmouseout = function () {
         go();
     };
-
 }
